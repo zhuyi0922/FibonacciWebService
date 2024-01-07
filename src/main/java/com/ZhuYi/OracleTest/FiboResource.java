a6f94e533824a63be8405dd6b29f6c3ecae548f7
zhuyi0922 <zhuy71302@gmail.com>
2024-01-07 20:45:46 +0800

    latest

--- src/main/java/com/ZhuYi/OracleTest/FiboResource.java
+++ src/main/java/com/ZhuYi/OracleTest/FiboResource.java
@@ -1,53 +1,51 @@
 package com.ZhuYi.OracleTest;
 
 import com.codahale.metrics.annotation.Timed;
-import com.google.errorprone.annotations.Var;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.math.BigInteger;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
 @Path("/fibonacci")
 @Produces(MediaType.APPLICATION_JSON)
 public class FiboResource {
     public static class FibonacciInput {
-        private int n;
-        public int getN() {
+        private BigInteger n;
+        public BigInteger getN() {
             return n;
         }
-        public void setN(int n) {
+        public void setN(BigInteger n) {
             this.n = n;
         }
     }
     public static class CalculateFibo{
-        public static List<Integer> calculateFibo(int n){
-            if (n <= 0) {
+        public static List<BigInteger> calculateFibo(BigInteger n){
+            if (n.compareTo(BigInteger.ONE) < 0) {
                 return null;
             }
-            int[] dp = new int[n];
-            dp[0] = 0;
-            if (n > 1) {
-                dp[1] = 1;
+            BigInteger[] dp = new BigInteger[n.intValue()];
+            dp[0] = BigInteger.ZERO;
+            if (n.compareTo(BigInteger.ONE) > 0) {
+                dp[1] = BigInteger.ONE;
             }
-            for (int i = 2; i < n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
+            for (int i = 2; i < n.intValue(); i++) {
+                dp[i] = dp[i - 1].add(dp[i - 2]);
             }
-            return Arrays.asList(Arrays.stream(dp).boxed().toArray(Integer[]::new));
+            return Arrays.asList(dp);
         }
-        public static List<Integer> sortFibonacci(List<Integer> fibArray) {
-            List<Integer> evenList = new ArrayList<>();
-            List<Integer> oddList = new ArrayList<>();
+        public static List<BigInteger> sortFibonacci(List<BigInteger> fibArray) {
+            List<BigInteger> evenList = new ArrayList<>();
+            List<BigInteger> oddList = new ArrayList<>();
             for (int i = fibArray.size()-1; i >= 0; i--) {
-                if (fibArray.get(i) % 2 == 0) {
+                if (fibArray.get(i).mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
                     evenList.add(fibArray.get(i));
                 } else {
                     oddList.add(fibArray.get(i));
                 }
             }
 
-            List<Integer> sortedList = new ArrayList<>();
+            List<BigInteger> sortedList = new ArrayList<>();
             sortedList.addAll(evenList);
             sortedList.addAll(oddList);
             return sortedList;
@@ -57,11 +55,14 @@ public class FiboResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Timed
-    public Map<String, List<Integer>> calculateFibonacci(FibonacciInput input) {
-        int n = input.getN();
-        Map<String, List<Integer>> result = new HashMap<>();
-        List<Integer> fibArray = CalculateFibo.calculateFibo(n);
+    public Map<String, List<BigInteger>> calculateFibonacci(FibonacciInput input) {
+        BigInteger n = input.getN();
+        Map<String, List<BigInteger>> result = new HashMap<>();
+        List<BigInteger> fibArray = CalculateFibo.calculateFibo(n);
         result.put("list", fibArray);
+        if (fibArray == null) {
+            return result;
+        }
         result.put("sorted", CalculateFibo.sortFibonacci(fibArray));
         return result;
     }
